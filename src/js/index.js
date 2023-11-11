8aa4ecfb932989c33063bbc91cca534fe858d0b1
Vinicius Junqueira Moreira <viniciusit.moreira@gmail.com>
2023-11-11 17:10:53 +0100

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,34 @@
-const sectionImage = document.querySelectorAll('.image-woman')
+const imageWoman = document.querySelectorAll('.image-woman');
+const form = document.querySelector('form');
 
 const switchImage = () => {
   if(window.innerWidth > 768) {
-    sectionImage[0].classList.remove('switchImage')
-    sectionImage[1].classList.add('switchImage')
+    imageWoman[0].classList.remove('switchImage');
+    imageWoman[1].classList.add('switchImage');
   } else {
-    sectionImage[1].classList.remove('switchImage')
-    sectionImage[0].classList.add('switchImage')
+    imageWoman[1].classList.remove('switchImage');
+    imageWoman[0].classList.add('switchImage');
   }
-}
+};
 
-window.addEventListener('resize', switchImage)
-window.addEventListener('DOMContentLoaded', switchImage)
+const validationEmail = (e) => {
+  e.preventDefault()
+  const inputEmail = document.querySelector('#input-email').value;
+  const errorMessage = document.querySelector('.error')
+  const iconError = document.querySelector('.icon-error')
+
+  try {
+  if(inputEmail.match(/\w{2,}@[a-zA-Z]{2,}\.[a-zA-Z]{2,}/)){
+    console.log('success')
+  }else {
+    throw new Error()
+  }
+  }catch(error) {
+    errorMessage.style.display = 'block';
+    iconError.style.display = 'inline';
+  }
+};
+
+window.addEventListener('resize', switchImage);
+window.addEventListener('DOMContentLoaded', switchImage);
+form.addEventListener('submit', validationEmail);
